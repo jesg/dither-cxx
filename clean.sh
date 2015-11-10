176f3756a965e1edcc93821c77328a5e6cc06e15
jesg <gowanjason@gmail.com>
2015-11-10 04:04:57 -0600

    clean up more files

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rm -rf ./bin ./tests/CMakeFiles ./tests/Makefile ./src/CMakeFiles ./src/Makefile CMakeFiles  Makefile ./tests/cmake_install.cmake cmake_install.cmake
+rm -rf ./bin ./tests/CMakeFiles ./tests/Makefile ./src/CMakeFiles ./src/Makefile CMakeFiles  Makefile ./tests/cmake_install.cmake cmake_install.cmake ./lib/libdither.so
 
